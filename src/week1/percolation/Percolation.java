829c7029506d1f45f52010eb051a066d1aad71d2
mrohadiii <muhammad.rohadi@panin.co.id>
2022-12-15 16:36:41 +0700

    feat: percolation

---
+++ src/week1/percolation/Percolation.java
@@ -0,0 +1,142 @@
+package week1.percolation;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    private int gridSize;
+    private int gridSquare;
+    private int virtualTop;
+    private int virtualBot;
+    private int openSite;
+    private boolean[][] grid;
+    private WeightedQuickUnionUF wquGrid;
+    private WeightedQuickUnionUF wquFull;
+
+    public Percolation(int n) {
+        if (n <= 0)
+            throw new IllegalArgumentException("Argument must be positive number");
+
+        gridSize = n;
+        gridSquare = n * n;
+        virtualTop = gridSquare;
+        virtualBot = gridSquare + 1;
+        openSite = 0;
+        grid = new boolean[gridSize][gridSize];
+        wquGrid = new WeightedQuickUnionUF(gridSquare + 2);
+        wquFull = new WeightedQuickUnionUF(gridSquare + 1);
+    }
+
+    public void open(int row, int col) {
+        validate(row, col);
+
+        int shiftRow = row - 1;
+        int shiftCol = col - 1;
+        int site = getSite(row, col);
+
+        // Check whether the site is alerady open
+        if (isOpen(row, col)) {
+            return;
+        }
+
+        // Open the site
+        grid[shiftRow][shiftCol] = true;
+        openSite++;
+
+        // Check top row and union to virtual top
+        if (row == 1) {
+            wquGrid.union(virtualTop, site);
+            wquFull.union(virtualTop, site);
+        }
+
+        if (row == gridSize) {
+            wquGrid.union(virtualBot, site);
+        }
+
+        // Check left and union
+        if (isOnGrid(row, col - 1) && isOpen(row, col - 1)) {
+            wquGrid.union(site, getSite(row, col - 1));
+            wquFull.union(site, getSite(row, col - 1));
+        }
+
+        // Check right and union
+        if (isOnGrid(row, col + 1) && isOpen(row, col + 1)) {
+            wquGrid.union(site, getSite(row, col + 1));
+            wquFull.union(site, getSite(row, col + 1));
+        }
+
+        // Check up and union
+        if (isOnGrid(row - 1, col) && isOpen(row - 1, col)) {
+            wquGrid.union(site, getSite(row - 1, col));
+            wquFull.union(site, getSite(row - 1, col));
+        }
+
+        // Check down and union
+        if (isOnGrid(row + 1, col) && isOpen(row + 1, col)) {
+            wquGrid.union(site, getSite(row + 1, col));
+            wquFull.union(site, getSite(row + 1, col));
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        validate(row, col);
+        return grid[row - 1][col - 1];
+    }
+
+    public boolean isFull(int row, int col) {
+        validate(row, col);
+        return wquFull.find(virtualTop) == wquFull.find(getSite(row, col));
+    }
+
+    public int numberOfOpenSites() {
+        return openSite;
+    }
+
+    public boolean percolates() {
+        return wquGrid.find(virtualTop) == wquGrid.find(virtualBot);
+    }
+
+    private void validate(int row, int col) {
+        if (!isOnGrid(row, col))
+            throw new IllegalArgumentException("Number out of bonds");
+    }
+
+    private boolean isOnGrid(int row, int col) {
+        int shiftRow = row - 1;
+        int shiftCol = col - 1;
+        return (shiftRow >= 0 && shiftRow < gridSize && shiftCol >= 0 && shiftCol < gridSize);
+    }
+
+    private int getSite(int row, int col) {
+        validate(row, col);
+        return (gridSize * (row - 1) + col) - 1;
+    }
+
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+        int row = 0;
+        int col = 0;
+
+        int numOpenSite = 1;
+        int testRow = 1;
+        int testCol = 1;
+        if (args.length > 0) {
+            numOpenSite = Integer.parseInt(args[0]);
+            testRow = Integer.parseInt(args[1]);
+            testCol = Integer.parseInt(args[2]);
+        }
+
+        Percolation perc = new Percolation(n);
+        while (!StdIn.isEmpty()) {
+            row = StdIn.readInt();
+            col = StdIn.readInt();
+            perc.open(row, col);
+
+            if (perc.numberOfOpenSites() == numOpenSite) {
+                StdOut.println("Is full: " + perc.isFull(testRow, testCol));
+                StdOut.println("Is percolate: " + perc.percolates());
+            }
+        }
+    }
+}
---
+++ src/week1/percolation/PercolationStats.java
@@ -0,0 +1,79 @@
+package week1.percolation;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+// import edu.princeton.cs.algs4.Stopwatch;
+
+public class PercolationStats {
+    private static final double CONFIDENCE_95 = 1.96;
+    private int gridSize;
+    private int trials;
+    private double[] trialsResult;
+
+    // Perform independent trials on an n by n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException("Number must be positive integer");
+        }
+
+        gridSize = n;
+        this.trials = trials;
+        trialsResult = new double[trials];
+
+        runExperiment();
+    }
+
+    // Run the experiment based on number of trials
+    private void runExperiment() {
+        for (int i = 0; i < trials; i++) {
+            Percolation perc = new Percolation(gridSize);
+            while (!perc.percolates()) {
+                int row = StdRandom.uniformInt(1, gridSize + 1);
+                int col = StdRandom.uniformInt(1, gridSize + 1);
+                perc.open(row, col);
+            }
+            int openSite = perc.numberOfOpenSites();
+            double result = (double) openSite / (gridSize * gridSize);
+            trialsResult[i] = result;
+        }
+    }
+
+    // Sample mean of percolation <em>threshold</em>
+    public double mean() {
+        return StdStats.mean(trialsResult);
+    }
+
+    // Determine standard deviation of percolation <em>threshold</em>
+    public double stddev() {
+        return StdStats.stddev(trialsResult);
+    }
+
+    public double confidenceLo() {
+        return mean() - ((CONFIDENCE_95 * stddev()) / Math.sqrt(trials));
+    }
+
+    public double confidenceHi() {
+        return mean() + ((CONFIDENCE_95 * stddev()) / Math.sqrt(trials));
+    }
+
+    // test client
+    public static void main(String[] args) {
+        int n = 200;
+        int trials = 100;
+        if (args.length > 1) {
+            n = Integer.parseInt(args[0]);
+            trials = Integer.parseInt(args[1]);
+        }
+
+        // Stopwatch stopwatch = new Stopwatch();
+        PercolationStats percStats = new PercolationStats(n, trials);
+        StdOut.printf("mean                     = %.7f%n", percStats.mean());
+        StdOut.printf("stddev                   = %.17f%n", percStats.stddev());
+        StdOut.printf("95%% confidence interval  = [%.15f, %.15f]%n",
+                percStats.confidenceLo(), percStats.confidenceHi());
+
+        // double timeElapse = stopwatch.elapsedTime();
+        // StdOut.printf("Total time elapse: %.9f milisecond)", timeElapse);
+    }
+}
